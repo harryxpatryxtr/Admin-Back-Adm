9c7c792277180e8d3a88d990b780f6d1d032209f
sAmir <samir.huaman@umatechnology.io>
2025-11-24 19:46:40 -0500

    CORS habilitado

--- src/app.js
+++ src/app.js
@@ -3,11 +3,12 @@ const koaLogger = require('koa-logger');
 const routes = require('./routes');
 const bodyParser = require('koa-bodyparser');
 const errorMiddleware = require('./middlewares/error.middleware');
-
+const cors = require('@koa/cors');
 const app = new Koa();
 
 // Middlewares globales
 app.use(errorMiddleware);
+app.use(cors());
 app.use(koaLogger());
 app.use(bodyParser());
 // Rutas
